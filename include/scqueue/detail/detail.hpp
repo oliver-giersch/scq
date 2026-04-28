71e896f74f8456d912f62ee91681b4b45be63db4
Oliver Giersch <oliver.giersch@b-tu.de>
2026-04-28 06:41:01 +0200

    fix something

--- include/scqueue/detail/detail.hpp
+++ include/scqueue/detail/detail.hpp
@@ -7,97 +7,104 @@
 
 #include <iostream>
 
-namespace scq::detail {
-template <bool finalize = false>
-struct finalize_bit_t {
-  static constexpr auto bit  = std::uintmax_t{ 0 };
-  static constexpr auto mask = ~bit;
-};
+namespace scq::detail
+{
+  template <bool finalize = false>
+  struct finalize_bit_t
+  {
+    static constexpr auto bit = std::uintmax_t{0};
+    static constexpr auto mask = ~bit;
+  };
 
-template <>
-struct finalize_bit_t<true> {
-  static constexpr std::uintmax_t bit =
-      std::uintmax_t{ 1 } << (std::numeric_limits<std::uintmax_t>::digits - 1);
-  static constexpr auto mask = ~bit;
-};
+  template <>
+  struct finalize_bit_t<true>
+  {
+    static constexpr std::uintmax_t bit =
+        std::uintmax_t{1} << (std::numeric_limits<std::uintmax_t>::digits - 1);
+    static constexpr auto mask = ~bit;
+  };
 
-struct cycle_t {
-  std::uintmax_t val;
-};
+  struct cycle_t
+  {
+    std::uintmax_t val;
+  };
 
-constexpr auto
-operator<=(const cycle_t& lhs, const cycle_t& rhs) {
-  return static_cast<std::intmax_t>(lhs.val) - static_cast<std::intmax_t>(rhs.val) <= 0;
-}
-
-constexpr auto
-operator<(const cycle_t& lhs, const cycle_t& rhs) {
-  return static_cast<std::intmax_t>(lhs.val) - static_cast<std::intmax_t>(rhs.val) < 0;
-}
-
-constexpr auto
-operator>=(const cycle_t& lhs, const cycle_t& rhs) {
-  return static_cast<std::intmax_t>(lhs.val) - static_cast<std::intmax_t>(rhs.val) >= 0;
-}
-
-template <typename T>
-struct pair_t {
-  using pointer = T*;
-
-  std::uintmax_t tag;
-  pointer        ptr;
-};
-
-template <typename T>
-struct alignas(16) atomic_pair_t {
-  using pointer = T*;
-
-  std::atomic<std::uintmax_t> tag{ 0 };
-  std::atomic<pointer>        ptr{ nullptr };
-
-  bool
-  compare_exchange_weak(
-      pair_t<T>& expected,
-      pair_t<T>  desired,
-      std::memory_order success,
-      std::memory_order failure
-  ) {
-    (void) success;
-    (void) failure;
-    uint8_t res;
-    asm volatile(
-      "lock cmpxchg16b %0"
-      : "+m"(*this), "=@ccz"(res), "+a"(expected.tag), "+d"(expected.ptr) // input ops
-      : "b"(desired.tag), "c"(desired.ptr)                                // output ops
-      : "memory"                                                          // clobbers
-    );
-
-    return res != 0;
+  constexpr auto
+  operator<=(const cycle_t &lhs, const cycle_t &rhs)
+  {
+    return static_cast<std::intmax_t>(lhs.val) - static_cast<std::intmax_t>(rhs.val) <= 0;
   }
 
-  pair_t<T>
-  fetch_and(pair_t<T> pair, std::memory_order order) {
-    auto curr = pair_t {
-      this->tag.load(std::memory_order_relaxed),
-      this->ptr.load(std::memory_order_relaxed)
-    };
+  constexpr auto
+  operator<(const cycle_t &lhs, const cycle_t &rhs)
+  {
+    return static_cast<std::intmax_t>(lhs.val) - static_cast<std::intmax_t>(rhs.val) < 0;
+  }
 
-    while (true) {
-      const auto next = pair_t {
-          curr.tag & pair.tag,
-          reinterpret_cast<pointer>(
-              reinterpret_cast<std::uintptr_t>(curr.ptr)
-              & reinterpret_cast<std::uintptr_t>(pair.ptr)
-          )
-      };
+  constexpr auto
+  operator>=(const cycle_t &lhs, const cycle_t &rhs)
+  {
+    return static_cast<std::intmax_t>(lhs.val) - static_cast<std::intmax_t>(rhs.val) >= 0;
+  }
 
-      if (this->compare_exchange_weak(curr, next, order, std::memory_order_relaxed)) {
-        return curr;
+  template <typename T>
+  struct pair_t
+  {
+    using pointer = T *;
+
+    std::uintmax_t tag;
+    pointer ptr;
+  };
+
+  template <typename T>
+  struct alignas(16) atomic_pair_t
+  {
+    using pointer = T *;
+
+    std::atomic<std::uintmax_t> tag{0};
+    std::atomic<pointer> ptr{nullptr};
+
+    bool
+    compare_exchange_weak(
+        pair_t<T> &expected,
+        pair_t<T> desired,
+        std::memory_order success,
+        std::memory_order failure)
+    {
+      (void)success;
+      (void)failure;
+      uint8_t res;
+      asm volatile(
+          "lock cmpxchg16b %0"
+          : "+m"(*this), "=@ccz"(res), "+a"(expected.tag), "+d"(expected.ptr) // input ops
+          : "b"(desired.tag), "c"(desired.ptr)                                // output ops
+          : "memory"                                                          // clobbers
+      );
+
+      return res != 0;
+    }
+
+    pair_t<T>
+    fetch_and(pair_t<T> pair, std::memory_order order)
+    {
+      auto curr = pair_t{
+          this->tag.load(std::memory_order_relaxed),
+          this->ptr.load(std::memory_order_relaxed)};
+
+      while (true)
+      {
+        const auto next = pair_t{
+            curr.tag & pair.tag,
+            reinterpret_cast<pointer>(
+                reinterpret_cast<std::uintptr_t>(curr.ptr) & reinterpret_cast<std::uintptr_t>(pair.ptr))};
+
+        if (this->compare_exchange_weak(curr, next, order, std::memory_order_relaxed))
+        {
+          return curr;
+        }
       }
     }
-  }
-};
+  };
 }
 
 #endif /* SCQ_DETAIL_HPP */
-
